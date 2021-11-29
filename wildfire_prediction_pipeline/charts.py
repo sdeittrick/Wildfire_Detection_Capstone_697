c321a1d7b89339b8822f4a542831a183fb94ddf7
Nick Essner <npessner@umich.edu>
2021-11-29 10:33:27 -0600

    integrating hyperparameter into scott workflow

--- wildfire_prediction_pipeline/charts.py
+++ wildfire_prediction_pipeline/charts.py
@@ -1,26 +1,29 @@
 import pandas as pd
 import numpy as np
 import altair as alt
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def line_chart(history):
 
     history_df = pd.DataFrame(history.history).reset_index()
     history_df = history_df.rename(columns={'index':'epoch'})
-    accuracy_df = pd.melt(history_df, id_vars=['epoch'], value_vars=['accuracy', 'val_accuracy'])
-    loss_df = pd.melt(history_df, id_vars=['epoch'], value_vars=['loss', 'val_loss'])
-    history_df = pd.melt(history_df, id_vars=['epoch'], value_vars=['accuracy', 'val_accuracy', 'loss', 'val_loss'])
+    accuracy_df = pd.melt(history_df, id_vars=['epoch'], value_vars=['accuracy', 'precision_m','recall_m','auc'])
+    loss_df = pd.melt(history_df, id_vars=['epoch'], value_vars=['loss'])
+    history_df = pd.melt(history_df, id_vars=['epoch'], value_vars=['accuracy', 'precision_m','recall_m', 'auc', 'loss'])
+    display(history_df.head())
 
     performanceChart = alt.Chart(history_df,title='Model performance').mark_line(size=3).encode(
         x=alt.X('epoch',axis=alt.Axis(title='Epoch', grid=False,tickCount=10)),
         y=alt.Y('value',axis=alt.Axis(title='Accuracy/Loss',format='%')),
-        color=alt.Color('variable',scale=alt.Scale(range=['#6f0000','#696969','#ff7b7b','#999999']),
-        legend=alt.Legend(title=None,labelFont='Cambria',labelColor='#696969',labelFontSize=14))
+        color=alt.Color('variable',scale=alt.Scale(range=['#6f0000','#696969','#ff7b7b','#CC5500','#458205']),
+        legend=alt.Legend(title=None,labelFont='Arial',labelColor='#696969',labelFontSize=14))
         ).properties(
         width=800,
         height=300
-        ).configure_title(fontSize=30,color='#232b2b',font='Cambria',anchor='start',offset=20
-        ).configure_axis(labelColor='#696969',labelFont='Cambria',labelFontSize=14,titleFont='Cambria',titleFontSize=16,titleColor='#696969'
+        ).configure_title(fontSize=30,color='#232b2b',font='Arial',anchor='start',offset=20
+        ).configure_axis(labelColor='#696969',labelFont='Arial',labelFontSize=14,titleFont='Arial',titleFontSize=16,titleColor='#696969'
         )#.configure_legend(labelFontStyle='Cambria',labelFontSize=12)
     
     #save as png
--- wildfire_prediction_pipeline/train.py
+++ wildfire_prediction_pipeline/train.py
@@ -6,6 +6,7 @@ import pickle
 import PIL
 from PIL import Image
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models, preprocessing
@@ -13,6 +14,32 @@ from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
+from tensorboard.plugins.hparams import api as hp
+from keras import backend as K
+
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+def areaUnderCurve(y_true, y_pred):
+    areaUnder = metrics.auc(y_true, y_pred)
+    return areaUnder
 
 
 def test_train_split(data):
@@ -30,7 +57,17 @@ def test_train_split(data):
     
     return train_images, train_labels, dev_images, dev_labels, test_images, test_labels
 
+def augment_layers(input_shape):
 
+    augLayers =[
+    layers.RandomFlip("vertical",input_shape=input_shape),
+    layers.RandomRotation(0.1),
+    # layers.RandomZoom(0.1),
+    #other options we can run with.. black and white, saturation, brightness, etc...
+    # layers.RandomContrast(1.0, seed=100),
+    ]
+
+    return keras.Sequential(augLayers)
 
 def cnn_model(data):
     
@@ -66,6 +103,71 @@ def cnn_model(data):
 
     history = model.fit(train_images, train_labels, epochs=epochs, 
                         validation_data=(test_images, test_labels))
+    return model
+
+
+def train_test_model(hparams,epochs,input_shape,train_images,train_labels,test_images, test_labels,
+                    HP_NUM_UNITS, HP_DROPOUT, HP_OPTIMIZER,
+                    params, losses, accuracies, f1_scores, precisions, recalls, cms,
+                    units, dropouts, optimizers, histories, augmentModel=False):
+    
+    if augmentModel:
+        data_augmentation = augment_layers(input_shape)
+    else:
+        data_augmentation = ''
+    
+    model = tf.keras.models.Sequential([
+    data_augmentation,
+    # tf.keras.layers.Rescaling(1./255),
+    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+#     tf.keras.layers.Dropout(hparams[HP_DROPOUT]),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(hparams[HP_NUM_UNITS], activation=tf.nn.relu),
+    tf.keras.layers.Dropout(hparams[HP_DROPOUT]),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax),
+    ])
+
+    model.compile(
+      optimizer=hparams[HP_OPTIMIZER],
+      loss='sparse_categorical_crossentropy',
+#       metrics=['accuracy'],
+      metrics=['accuracy',f1_m,precision_m, recall_m]#,areaUnderCurve]
+    )
+    
+    model.fit(train_images, train_labels, epochs=epochs)
+    history = model.fit(train_images, train_labels, epochs=epochs)
+    histories.append(model.history)
+    loss, accuracy, f1_score, precision, recall= model.evaluate(test_images, test_labels) #, areaUnder 
+    losses.append(loss)
+    accuracies.append(accuracy)
+    f1_scores.append(f1_score)
+    precisions.append(precision)
+    recalls.append(recall)
+    # aucs.append(areaUnder)
+    
+    predictions = model.predict(x=test_images, steps=len(test_images), verbose=0)
+    cm = confusion_matrix(y_true=test_labels, y_pred=np.argmax(predictions, axis=-1))
+    cms.append(cm)
+    
+    return loss, accuracy, f1_score, precision, recall#, areaUnder
+
+def run(run_dir, hparams, epochs, input_shape,train_images,train_labels,test_images, test_labels,
+        HP_NUM_UNITS, HP_DROPOUT, HP_OPTIMIZER, 
+        params, losses, accuracies, f1_scores, precisions, recalls, cms,
+        units, dropouts, optimizers, histories, augmentModel=False):
+
+    with tf.summary.create_file_writer(run_dir).as_default():
+        hp.hparams(hparams)  # record the values used in this trial
+
+        model = train_test_model(hparams,epochs,input_shape,train_images,train_labels,test_images, test_labels,
+                                HP_NUM_UNITS, HP_DROPOUT, HP_OPTIMIZER,
+                                params, losses, accuracies, f1_scores, precisions, recalls, cms,
+                                units, dropouts, optimizers, histories, augmentModel)
     return model
 
 if __name__ == '__main__':
